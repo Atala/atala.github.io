d400a32acb7c9e810508c8eff5b402c43e6f4528
Atala <alois.guillope@gmail.com>
2017-05-12 18:21:23 +0200

    feat: add scripts

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -82,6 +82,28 @@ gulp.task('images', () =>
     .pipe($.size({title: 'images'}))
 );
 
+// Concatenate and minify JavaScript. Optionally transpiles ES2015 code to ES5.
+// to enable ES2015 support remove the line `"only": "gulpfile.babel.js",` in the
+// `.babelrc` file.
+gulp.task('scripts', () =>
+    gulp.src([
+      // Note: Since we are not using useref in the scripts build pipeline,
+      //       you need to explicitly list your scripts here in the right order
+      //       to be correctly concatenated
+      './app/scripts/main.js'
+      // Other scripts
+    ])
+      .pipe($.sourcemaps.init())
+      .pipe($.babel())
+      .pipe($.sourcemaps.write())
+      .pipe($.concat('main.min.js'))
+      .pipe($.uglify({preserveComments: 'some'}))
+      // Output files
+      .pipe($.size({title: 'scripts'}))
+      .pipe($.sourcemaps.write('.'))
+      .pipe(gulp.dest('docs/scripts'))
+);
+
 
 gulp.task('serve', ['styles', 'images'], function () {
     browserSync({
@@ -108,6 +130,6 @@ gulp.task('clean', () => del(['.tmp', 'docs/*', '!docs/.git'], {dot: true}));
 gulp.task('default', ['clean'], () =>
   runSequence(
     'styles',
-    ['html', 'images', 'copy']
+    ['html', 'images', 'copy', 'scripts']
   )
 );
