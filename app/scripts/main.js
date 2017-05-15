8a119a3261eaea16938d86278941526fc3e2b519
Atala <alois.guillope@gmail.com>
2017-05-15 12:51:18 +0200

    feat: add projects section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,9 +5,14 @@
 document.addEventListener('DOMContentLoaded', function() {
 
     document.addEventListener('scroll', function () {
-        var skillsBannerTop = document.querySelector('.banner--skills').getBoundingClientRect().top;
+        var skillsBannerTop = document.querySelector('.banner--skills').getBoundingClientRect().top,
+            projectsBannerTop = document.querySelector('.banner--projects').getBoundingClientRect().top;
         Array.prototype.forEach.call(document.querySelectorAll('.nav__item'), function (item) { item.classList.remove('nav__item--active');});
-        if (skillsBannerTop < document.querySelector('.header').offsetHeight + 1 && skillsBannerTop > -514) {
+
+        if (projectsBannerTop < document.querySelector('.header').offsetHeight + 2) {
+            document.querySelector('#projects-nav').classList.add('nav__item--active');
+        }
+        else if (skillsBannerTop < document.querySelector('.header').offsetHeight + 1) {
             document.querySelector('#skills-nav').classList.add('nav__item--active');
         }
     });
