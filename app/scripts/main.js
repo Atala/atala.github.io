8bd4635267a6f844943b5d0ec36596056412e9dd
Atala <alois.guillope@gmail.com>
2017-05-15 11:09:21 +0200

    clean: improve scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,18 @@
+/* globals smoothScroll */
+
 'use strict';
 
-document.addEventListener('scroll', function () {
-    var skillsBannerTop = document.querySelector('.banner--skills').getBoundingClientRect().top;
-    Array.prototype.forEach.call(document.querySelectorAll('.nav__item'), function (item) { item.classList.remove('nav__item--active');});
-    if (skillsBannerTop < 55 && skillsBannerTop > -514) {
-        document.querySelector('#skills-nav').classList.add('nav__item--active');
-    }
-});
+document.addEventListener('DOMContentLoaded', function() {
 
+    document.addEventListener('scroll', function () {
+        var skillsBannerTop = document.querySelector('.banner--skills').getBoundingClientRect().top;
+        Array.prototype.forEach.call(document.querySelectorAll('.nav__item'), function (item) { item.classList.remove('nav__item--active');});
+        if (skillsBannerTop < document.querySelector('.header').offsetHeight + 1 && skillsBannerTop > -514) {
+            document.querySelector('#skills-nav').classList.add('nav__item--active');
+        }
+    });
+
+    smoothScroll.init({
+        selectorHeader: '.header'
+    });
+});
